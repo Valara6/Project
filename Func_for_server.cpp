8ad98028bcbfa24e5478e979626a8d1322773452
val den <valerad2212@mail.ru>
2023-03-09 15:26:04 +0300

    fix parsing

--- Func_for_server.cpp
+++ Func_for_server.cpp
@@ -11,26 +11,39 @@ void Registration(QString email,QString username, QString password){
  qDebug()<<"Mail= "<<email<<" name="<<username<<" pass="<<password;
 }
 void Parsing(QByteArray message){
-    QList<QByteArray> parts = message.split(' ');
+//data format: funk_name$dat1$dat2$...
+    QList<QByteArray> parts = message.split('$');
     //for(const QByteArray& part : parts){
         //QList<QByteArray> pair = part.split('=');
-        if(parts.count()==2){
+        qDebug()<<parts[0];
+        if(parts[0]=="login"){
+            if (parts.count()!=3){
+                qDebug()<<"Unvalid data for "<<parts[0];
+            }
+            else{
             //Разделить имя и пароль
-            QString name = parts[0];
-            QString pass= parts[1];
-            //Убрать \r\n у pass
-            Login(name,pass);
-        }
-        else if (parts.count()==3){
-            //Разделить почту, имя, пароль
-            QString mail=parts[0];
             QString name = parts[1];
             QString pass= parts[2];
             //Убрать \r\n у pass
+            Login(name,pass);
+            }
+        }
+        else if (parts[0]=="registration"){
+            //Разделить почту, имя, пароль
+            if (parts.count()!=4){
+                qDebug()<<"Unvalid data for "<<parts[0];
+            }
+            else
+            {
+            QString mail=parts[1];
+            QString name = parts[2];
+            QString pass= parts[3];
+            //Убрать \r\n у pass
             Registration(mail,name,pass);
+            }
         }
         else{
-            qDebug()<< "Invalid Data";
+            qDebug()<< "Invalid func name";
         }
     }
 
--- Funcs_for_server.h
+++ Funcs_for_server.h
@@ -1,17 +1,12 @@
 #ifndef FUNCS_FOR_SERVER_H
 #define FUNCS_FOR_SERVER_H
 #include <QObject>
-#include <QTcpServer>
-#include <QTcpSocket>
 
-#include <QtNetwork>
-#include <QByteArray>
-#include <QDebug>
-
-#include <QVector>
 
 void Parsing(QByteArray message);
-void Login(QString username, QString password);
+void Login(QString username, QString password);//log + pass
 void Registration(QString email,QString username, QString password);
-
+void CheckResult(); //task + input+ + answer
+void Statistic_stud();
+void Statistic_teach();
 #endif // FUNCS_FOR_SERVER_H
